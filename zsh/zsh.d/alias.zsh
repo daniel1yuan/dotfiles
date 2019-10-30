33f12664b912a06fa2c3c15c5ecc86ed7be2339b
Daniel Yuan <daniel1yuan@gmail.com>
2019-10-30 10:22:48 -0700

    Updating gitignore and adding go and reload alias

--- zsh/zsh.d/alias.zsh
+++ zsh/zsh.d/alias.zsh
@@ -3,5 +3,6 @@ alias cdProj="cd ~/Projects/ProjManager"
 alias cmaster="git checkout master"
 alias pmaster="git pull origin master"
 alias ls='ls --color=auto'
+alias reload="source ~/.zshrc"
 
 alias ta='task add project:$TASK_CONTEXT'
--- zsh/zsh.d/third_party.zsh
+++ zsh/zsh.d/third_party.zsh
@@ -1,6 +1,10 @@
+# Adding NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/danielyuan/.anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/danielyuan/.anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
@@ -13,6 +17,5 @@ fi
 unset __conda_setup
 # <<< conda initialize <<<
 
-# Adding NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# Adding Go
+export PATH=$PATH:/usr/local/go/bin
