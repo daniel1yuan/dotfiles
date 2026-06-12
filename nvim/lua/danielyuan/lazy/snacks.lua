dd67cbe6ce22d5010f2ed01aa834de1d677fcc25
Daniel Yuan <daniel1yuan@gmail.com>
2026-06-12 09:12:16 -0700

    feat: Adding lazygit with catppuccin theme and a nvim keybind

--- nvim/lua/danielyuan/lazy/snacks.lua
+++ nvim/lua/danielyuan/lazy/snacks.lua
@@ -17,8 +17,33 @@ return {
     },
   },
   keys = {
-    { "<leader>.", function() Snacks.scratch() end, desc = "Toggle scratch buffer" },
-    { "<leader>>", function() Snacks.scratch.select() end, desc = "Select scratch buffer" },
-    { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification history" },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>.",
+      function()
+        Snacks.scratch()
+      end,
+      desc = "Toggle scratch buffer",
+    },
+    {
+      "<leader>>",
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = "Select scratch buffer",
+    },
+    {
+      "<leader>n",
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = "Notification history",
+    },
   },
 }
